cbd7b3cdc91549fe3daa993c47399099142250f6
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-18 21:38:40 -0600

    Implemented validation of SECURITY_CODE to show error message and located State changes properly to hhide error message

--- src/UseState.js
+++ src/UseState.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+const SECURITY_CODE = 'paradigma';
+
 function UseState({name}) {
-  const [error, setError] = React.useState(true);
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+
+  console.log(value);
 
   React.useEffect(() => {
     console.log("Empezando el efecto");
@@ -10,7 +15,10 @@ function UseState({name}) {
     if(!!loading){                                                    // Cambiamos el estado de 'loading' a false si el loading esta cargando, esto lo hacemos simulando una petición a un servidor usando un setTimeout de 2000 milisegundos
       setTimeout(() => {
         console.log("Haciendo la validación");
-        setLoading(false);
+
+        if(value !== SECURITY_CODE) setError(true);                   // Mostramos el error si el código no es correcto al security code establecido
+        setLoading(false);                                            // Siempre que terminamos de hacer la validación, cambiamos el estado de 'loading' a false
+
         console.log("Terminando la validación");
       }, 2000);
     }
@@ -24,13 +32,20 @@ function UseState({name}) {
       <h2>Delete {name}</h2>
       <p>Please, write the security code to probe that you want to delete</p>
 
-      { error && (<p>Error: code is incorrect</p>) }
+      { (error && !loading) && (<p>Error: code is incorrect</p>) }
 
       { loading && (<p>Loading...</p>) }
 
-      <input placeholder='Security Code' />
+      <input 
+        placeholder='Security Code' 
+        value={value}
+        onChange={(event) => { setValue(event.target.value) }}        // Cambiamos el valor del State value usando la función 'setValue' cuando el usuario escriba en el input con el valor dentro del input
+      />
       <button
-        onClick={() => setLoading(true)}                              // Usamos la función 'setError' para cambiar el estado de 'error' a su valor contrario
+        onClick={() => { 
+          setLoading(true);                                     // Usamos el Hook 'useState' para cambiar el estado de 'loading' a true
+          setError(false);                                      // Cada vez que validemos el código, vamos a cambiar el estado de 'error' a false
+        }}                              
       >Validate</button>
     </div>
   );
