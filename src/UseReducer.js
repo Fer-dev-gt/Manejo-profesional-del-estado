06265c08ce9683178cb739ff1990834463dc2dac
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-20 21:34:35 -0600

    Modified actionTypes so it implements Action creator so we have a more declarative code

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const SECURITY_CODE = 'paradigma';
 
-// Creamos un Objeto que contiene el estado inicial del Componente, es un State complejo porque tiene varios estados dentro de un Objeto
-const initialState = {
+const initialState = {                                                                      // Creamos un Objeto que contiene el estado inicial del Componente, es un State complejo porque tiene varios estados dentro de un Objeto
   value: '',
   error: false,
   loading: false,
@@ -11,7 +10,7 @@ const initialState = {
   confirmed: false
 };
 
-const actionTypes = {
+const actionTypes = {                                                                       // Creamos un Objeto que contiene las acciones que se van a ejecutar en el 'reducer' asi al escribir el nombre de la acción en el 'dispatch' no nos equivocamos de nombre
   error: 'ERROR',
   check: 'CHECK',
   write: 'WRITE',
@@ -21,8 +20,8 @@ const actionTypes = {
   finish_loading: 'FINISH_LOADING'
 }
 
-const reducerObject = (state, payload) => ({                                     // Usamos un Objeto para evaluar la acción (action.type) que se va a ejecutar, le retornamos implicitamente al Objeto usando '()' en vez de '{}' y así ya no escribir return
-  [actionTypes.confirm]: { ...state, error: false, loading: false, confirmed: true },
+const reducerObject = (state, payload) => ({                                                 // Usamos un Objeto para evaluar la acción (action.type) que se va a ejecutar, le retornamos implicitamente al Objeto usando '()' en vez de '{}' y así ya no escribir return
+  [actionTypes.confirm]: { ...state, error: false, loading: false, confirmed: true },        // El key lo obtenemos al ejecutar [action.type], esto con el fin de evitar escribir el nombre de la acción en el key y así evitar errores de escritura
   [actionTypes.error]: { ...state, error: true, loading: false },                            // El formato que usamos será de key: value, donde la key será el nombre de la acción y el value será el estado que se va a retornar
   [actionTypes.check]: { ...state, loading: true },
   [actionTypes.write]: { ...state, value: payload },
@@ -43,7 +42,16 @@ const reducer = (state, action) => {
 function UseReducer({ name }) {
   const [state, dispatch] = React.useReducer(reducer, initialState);              // Usamos el Hook 'useReducer' para crear un solo 'state' que maneja varios estados, el cual recibe un reducer y un estado inicial, y retorna un estado y un dispatch, el cual es una función que se usa para ejecutar las acciones que se van a ejecutar en el 'reducer', el 'dispatch' va a reemplazar al 'setState' que usamos en el 'useState'
 
-
+  // Empleamos los 'Action Creator', Decidí hacerlo más corto y dinámico al usar Action Creators con Arrow Functions guardadas dentro de variables para cambiar los states
+  const onConfirm = () => dispatch({ type: actionTypes.confirm });
+  const onError = () => dispatch({ type: actionTypes.error });
+  const onCheck = () => dispatch({ type: actionTypes.check });
+  const onDelete = () => dispatch({ type: actionTypes.delete });
+  const onReset = () => dispatch({ type: actionTypes.reset });
+  const onFinishLoading = () => dispatch({ type: actionTypes.finish_loading });
+  const onWrite = (event) => {                                                    // Tambien podemos escribir las Arrow Functions de esta manera, el valor de 'event' ya lo obtiene la función debido a que es un evento que se ejecuta en el input
+    dispatch({ type: actionTypes.write, payload: event.target.value });           // Usamos el 'dispatch' para ejecutar las acciones que se van a ejecutar en el 'reducer', el cual recibe un Objeto que contiene el 'action.type' y el 'action.payload', el 'action.payload' es el valor que se va a cambiar en el state
+  }
 
   React.useEffect(() => {
     console.log("Empezando el efecto");
@@ -53,12 +61,12 @@ function UseReducer({ name }) {
         console.log("Haciendo la validación");
 
         if(state.value !== SECURITY_CODE) {
-          dispatch({ type: actionTypes.error });                                             // Usamos el 'dispatch' para ejecutar las acciones que se van a ejecutar en el 'reducer', en este caso ejecutamos la acción 'ERROR'
+          onError();
         } else {
-          dispatch({ type: actionTypes.confirm });                                           // Usamos el 'dispatch' para ejecutar las acciones que se van a ejecutar en el 'reducer', en este caso ejecutamos la acción 'CONFIRM'
+          onConfirm();                                                            
         }
 
-        dispatch({ type: actionTypes.finish_loading });                                      // Aplicamos el mismo concepto de arriba para cambiar el 'loading' a false despues de haber ejecutado otro 'dispatch', esto por temas de Scopes y Closures, es necesario para no sobreescrbir los otros estados que no estamos cambiando
+        onFinishLoading();                                                         
         console.log("Terminando la validación");
       }, 1000);
     }
@@ -80,15 +88,11 @@ function UseReducer({ name }) {
           placeholder='Security Code' 
           value={state.value}
           disabled={state.loading}                                                          // Deshabilitamos el input cuando el loading esta cargando y así evitar que el usuario pueda escribir en el input y cuando termine el loading se ponga el anterior
-          onChange={(event) => { 
-            dispatch({ type: actionTypes.write, payload: event.target.value });                       // Usamos el 'dispatch' para ejecutar las acciones que se van a ejecutar en el 'reducer', la acción es 'WRITE' y usamos el 'payload' para pasarle el valor del input
-          }}
+          onChange={onWrite}                                                                // No es necesario mandar el event, ya que el 'onWrite' ya lo recibe
         />
-        <button
-          onClick={() => { 
-            dispatch({ type: actionTypes.check });                                                    // Usamos el 'dispatch' para ejecutar las acciones que se van a ejecutar en el 'reducer', la acción es 'CHECK'
-          }}                              
-        >Validate</button>
+        <button onClick={onCheck}>
+          Validate
+        </button>
       </div>
     );
   } else if(!!state.confirmed && !state.deleted) {                                          // Si el State 'confirmed' es true y el State 'deleted' es false, entonces mostramos la pantalla para confirmar que se quiere eliminar
@@ -97,20 +101,10 @@ function UseReducer({ name }) {
         <div>
           <h2>Delete {name}</h2>
           <p>Are you sure you want to delete UseState</p>
-    
-          <button
-            onClick={() => {
-              dispatch({ type: actionTypes.delete });
-            }}
-          >
+          <button onClick={onDelete}>
             Yes, delete
           </button>
-
-          <button
-            onClick={() => {
-              dispatch({ type: actionTypes.reset });
-            }}
-          >
+          <button onClick={onReset}>
             No, go back
           </button>
         </div>
@@ -121,11 +115,7 @@ function UseReducer({ name }) {
       <>
         <div>
           <h2>{name} was deleted</h2>
-          <button
-            onClick={() => {
-              dispatch({ type: actionTypes.reset });
-            }}
-          >
+          <button onClick={onReset}>
             Go back, reset
           </button>
         </div>
