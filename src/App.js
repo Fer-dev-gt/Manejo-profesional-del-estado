2fc6dcf2bf6cf562a73ba1d189827345f0d528db
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-25 21:07:27 -0600

    fix added simple user instruction and code for changing states

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1></h1>
+      <h3>You just need to write 'paradigma' on the input (in lowercase)</h3>
       <UseReducer name="UseReducer with 'Reducer Object'"/>
       <UseState name="UseState"/>
       <ClassState name="ClassState"/>                                        {/* Componente creado usando Class que extends de 'React.Component', las propiedades se mandan de las misma manera que con un Componente por función */}
