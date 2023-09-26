04642537914a65bf162258c0234a1715fce212f8
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-25 19:48:02 -0600

    fix main page title to display describing name for web and mobile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { UseReducer } from './States/UseReducer';
 function App() {
   return (
     <div className="App">
-      <h1>States</h1>
+      <h1></h1>
       <UseReducer name="UseReducer with 'Reducer Object'"/>
       <UseState name="UseState"/>
       <ClassState name="ClassState"/>                                        {/* Componente creado usando Class que extends de 'React.Component', las propiedades se mandan de las misma manera que con un Componente por función */}
